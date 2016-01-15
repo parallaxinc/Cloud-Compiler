ba1ba845e1e2d6374b1f1d5597e789704c6bc9fc
michel-cf <michel@creatingfuture.eu>
2016-01-15 20:17:08 +0100

    Extended load test

--- locustfile.py
+++ locustfile.py
@@ -1,26 +1,50 @@
 from locust import HttpLocust, TaskSet, task
 
 
-single_spin_wait = "PUB Blinky\n  DIRA[16]~~\n  repeat\n    OUTA[16]~\n    waitcnt(clkfreq + cnt)\n    OUTA[16]~~\n    waitcnt(clkfreq + cnt)\n"
-single_spin_toggle = "PUB blinky\n  repeat\n    !outa[16]\n"
+single_spin = ("PUB Blinky\n"
+               "  DIRA[16]~~\n"
+               "  repeat\n"
+               "    OUTA[16]~\n"
+               "    waitcnt(clkfreq + cnt)\n"
+               "    OUTA[16]~~\n"
+               "    waitcnt(clkfreq + cnt)\n")
 
-
-single_c = '#include "simpletools.h"\nint main() {\n  while(1) {\n    high(16);\n    pause(1000);\n    low(16);\n    pause(1000);\n  }\n}\n'
+single_c = ('#include "simpletools.h"\n'
+            'int main() {\n'
+            '  while(1) {\n'
+            '    high(16);\n'
+            '    pause(1000);\n'
+            '    low(16);\n'
+            '    pause(1000);\n'
+            '  }\n'
+            '}\n')
 
 
 class CCompilerTaskSet(TaskSet):
 
     @task
-    def single_spin_wait(self):
-        self.client.post("/single/spin/compile", data=single_spin_wait)
+    def single_spin_compile(self):
+        self.client.post("/single/spin/compile", data=single_spin)
 
     @task
-    def single_spin_toggle(self):
-        self.client.post("/single/spin/compile", data=single_spin_toggle)
+    def single_spin_bin(self):
+        self.client.post("/single/spin/bin", data=single_spin)
 
     @task
-    def single_c(self):
+    def single_spin_eeprom(self):
+        self.client.post("/single/spin/eeprom", data=single_spin)
+
+    @task
+    def single_c_compile(self):
         self.client.post("/single/prop-c/compile", data=single_c)
+
+    @task
+    def single_c_bin(self):
+        self.client.post("/single/prop-c/bin", data=single_c)
+
+    @task
+    def single_c_eeprom(self):
+        self.client.post("/single/prop-c/eeprom", data=single_c)
 
 
 class CCompilerLocust(HttpLocust):
